8b4ff22329b952b66e5081d82dc691576f5c0092
Andres Ariel Alvarez <Dre@Andress-MacBook.local>
2016-05-05 17:51:39 -0700

    Checkers

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
-// Your JS goes here
+Your JS goes here
+for(var i=0;i<81;i++){
+var black = document.createElement('div');
+black.style.width='11.1%';
+black.style.float='left';
+black.style.paddingBottom='11.1%';
+    if(i%2) {
+        black.style.backgroundColor = 'black';
+        document.body.appendChild(black)
+    }
+    else {
+        black.style.backgroundColor='red';
+        document.body.appendChild(black)
+    }
+}
