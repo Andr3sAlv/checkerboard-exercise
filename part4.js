f38a917aeec0df1f74cac9896f6b3aa02f6d4563
Andres Ariel Alvarez <Dre@Andress-MacBook.local>
2016-06-14 18:31:22 -0700

    changes

--- part4.js
+++ part4.js
@@ -1,4 +1,4 @@
-//third part
+//fourth part
 var body = document.getElementsByTagName('body')[0];
 
 for(var i=0;i<81;i++){                                                          
@@ -10,21 +10,19 @@ for(var i=0;i<81;i++){
                                                                                 
   if(i%2) {                                                                     
            div.style.backgroundColor = 'rgba(0, 0, ' + Math.floor(hue) + ', 1)';  
-       }                                                                          
-     else{                                                                      
+       } else {                                                                      
               div.style.backgroundColor = 'rgba(0, ' + Math.floor(hue) + ',0 , 1)';  
           }                                                                          
      document.body.appendChild(div);                                            
  }                                                                              
-     document.body.appendChild(div);                                            
                                                                                      
 function changeColor() {
   for (var i=0;i<body.children.length; i++){
     var r = Math.floor(Math.random()*255) 
     var g = Math.floor(Math.random()*255) 
     var b = Math.floor(Math.random()*255) 
-    body.children[i].style.backgroundColor = 
+    body.children[i].style.backgroundColor = 'rgb('+r+','+g+','+b+')' 
   }
 }
 
-setInterval(changeColor, 2000);
+setInterval(changeColor, 1);
